e3e5af98ed6f2f5e866f9251c6830c712b92aa07
bladee <102807049+wraithyumming@users.noreply.github.com>
2024-11-03 17:26:21 +0200

    feat

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,10 @@ import Logo from '../../images/logo_dark.png'
 const Footer = () => {
     return (
         <div className={styles.footer}>
-
+            <div className={styles.container}>
+            <img src={Logo} alt="/" />
+            <button>Share A Car</button>
+            </div>
         </div>
     )
 }
